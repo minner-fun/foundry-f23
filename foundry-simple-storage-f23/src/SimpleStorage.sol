1eb41bab4d439839c6100df88a71689169e25d87
qfm_win <qifumin2017@163.com>
2026-01-18 23:04:12 +0800

    deployed

--- foundry-simple-storage-f23/src/SimpleStorage.sol
+++ foundry-simple-storage-f23/src/SimpleStorage.sol
@@ -1,12 +1,17 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.19;
+pragma solidity ^0.8.19;
 
-contract SimpleStorage{
+contract SimpleStorage {
+    address public Owner;
+
+    constructor() payable {
+        Owner = msg.sender;
+    }
+
     uint256 public favoriteNumber;
-
     uint256[] public listFavorite;
 
-    struct Persion{
+    struct Persion {
         string name;
         uint256 age;
     }
@@ -15,18 +20,18 @@ contract SimpleStorage{
 
     Persion[] public listPersion;
 
-    function store(uint256 _favoriteNumber) public{
+    function store(uint256 _favoriteNumber) public {
         favoriteNumber = _favoriteNumber;
         listFavorite.push(_favoriteNumber);
     }
 
-    function retrieve() public view returns(uint256){
+    function retrieve() public view returns (uint256) {
         return favoriteNumber;
     }
 
-    function createPersion(string memory _name, uint256 _age) public returns(Persion memory){
+    function createPersion(string memory _name, uint256 _age) public returns (Persion memory) {
         listPersion.push(Persion(_name, _age));
-        nameToOrder[_name] = listPersion.length -1; 
+        nameToOrder[_name] = listPersion.length - 1;
         return listPersion[listPersion.length - 1];
     }
 }
